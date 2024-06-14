f4aff133310ef8977138864c02263b0afc2a5aa6
Madison Pardy <Madison.Pardy@quest.com>
2024-06-13 23:40:20 -0300

    Building out import UI components

--- FC_UIBuilder.py
+++ FC_UIBuilder.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 
+
 class f_base(ctk.CTkFrame):
     """ Base class for FC frames."""
     def __init__(self, *args, **kwargs):
@@ -29,8 +30,6 @@ class f_import(f_main):
         self.f_detail = ctk.CTkFrame(self, corner_radius=0)
         self.f_detail.grid(row=0, column=0, sticky="nw")
 
-        # TEST
-        ctk.CTkLabel(self.f_detail, text="DISPLAYTEST").pack()
         self._mk_import_tabv(self.f_detail)
 
     def _mk_import_tabv(self, master):
@@ -42,15 +41,21 @@ class f_import(f_main):
         # Setup grid layouts for each tab
         self.import_mantab.grid_columnconfigure(0, weight=1)
         self.import_mantab.grid_rowconfigure(1, weight=1)
-        self.import_autotab.grid_columnconfigure(1, weight=1)
+        self.entr_flwg_man = fs_entry(lbl_text="Follower JSON: ", placeholder_text="Path/To/Followers.json", master=self.import_mantab)
+        self.entr_flwg_man.grid(row=0, column=0, sticky="nw")
+        self.entr_flwr_man = fs_entry(lbl_text="Following JSON: ", placeholder_text="Path/To/Following.json", master=self.import_mantab)
+        self.entr_flwr_man.grid(row=1, column=0, sticky="nw")
+
+        self.import_autotab.grid_columnconfigure(0, weight=1)
         self.import_autotab.grid_rowconfigure(2, weight=1)
-        testfsbtn = fs_entry(lbl_txt="Follower JSON: ", plchldr="Path/To/File.json", master=self.import_mantab)
-        testfsbtn.grid(row=1, column=0, sticky="nw")
+
+        self.entr_flwr_auto = label_entry(lbl_text="Follower Abbrv: ", placeholder_text="Follower File Abbrv", master=self.import_autotab)
+        self.entr_flwr_auto.grid(row=0, column=0, sticky="nw")
+
+        self.entr_flwg_auto = label_entry(lbl_text="Following Abbrv: ", placeholder_text="Following File Abbrv", master=self.import_autotab)
+        self.entr_flwg_auto.grid(row=1, column=0, sticky="nw")
 
         """
-        # Manual Tab
-        # 2 File selects
-
         # Auto Tab
         # Flwr/Flwg prefix
         # Detect Button
@@ -70,34 +75,39 @@ class f_import(f_main):
         """
 
 
-class fs_entry(f_base):
-    """ File/Dir entry component."""
+class label_entry(f_base):
+    """ Labled text entry."""
     def __init__(self,
-                 lbl_txt,
-                 plchldr="",
-                 dir_only=False,
+                 lbl_text,
+                 placeholder_text="",
                  *args,
                  **kwargs):
         super().__init__(*args, **kwargs)
-        self.selection = ""
-        # Add elements to frame
-        # ICON?
-        self.label = ctk.CTkLabel(self, text=lbl_txt).pack(side=ctk.TOP)
-        self.entry = ctk.CTkEntry(self, placeholder_text=plchldr).pack(side=ctk.TOP)
-        self.button = self._fs_button(dir_only=dir_only).pack(side=ctk.TOP)
+        self.label = ctk.CTkLabel(self, text=lbl_text)
+        self.label.pack(side=ctk.LEFT)
+        self.entry = ctk.CTkEntry(self, placeholder_text=placeholder_text)
+        self.entry.pack(side=ctk.LEFT)
 
-    def _file_select(self):
+
+class fs_entry(label_entry):
+    """ File/Dir entry component."""
+    def __init__(self,
+                 lbl_text,
+                 placeholder_text="",
+                 dir_only=False,
+                 btn_text="...",
+                 *args,
+                 **kwargs):
+        super().__init__(lbl_text=lbl_text, placeholder_text=placeholder_text, *args, **kwargs)
+        kwargs.pop("master")
+        self.dir_only = dir_only
+        self.button = ctk.CTkButton(master=self, text=btn_text, command=self._fs_select, **kwargs)
+        self.button.pack(side=ctk.LEFT)
+
+    def _fs_select(self):
         # Open file dialog and return selected file name
-        path = ctk.filedialog.askopenfilename()
-        self.entry.configure(require_redraw=True, placeholder_text=path)
+        path = ctk.filedialog.askdirectory() if self.dir_only else ctk.filedialog.askopenfilename()
+        if path:
+            self.entry.delete(0, -1)
+            self.entry.insert(0, path)
         return path
-
-    def _dir_select(self):
-        # Open file dialog and return selected folder name
-        path = ctk.filedialog.askdirectory()
-        self.entry.configure(require_redraw=True, placeholder_text=path)
-        return path
-
-    def _fs_button(self, dir_only=False, **kwargs):
-        cmd = self._dir_select if dir_only else self._file_select
-        return ctk.CTkButton(master=self, command=cmd, *kwargs)
